c747e868f3530e19eae5f034c4779af230cb9a1a
elvis-lee <lihao1242@gmail.com>
2017-03-06 15:18:33 -0500

    codes and results for subject01, results include decoding accuracy and time frequency decoding accuracy map for different groups

---
+++ codes/TFStep00_WaveletDecoding_Total.m
@@ -0,0 +1,130 @@
+clear; clc;
+addpath(genpath('Functions')); % add path of functions
+addpath(genpath('/dataslow/haoli/libsvm-3.22'));
+
+
+%% step 0: set parameters
+ProjectName = 'grating';  
+i_subject = 1;
+SubjectName = ['grating_' num2str(i_subject, '%.2d')];
+Freq_range = 10:2:80;
+condname = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18];
+results_location = ['../results/' SubjectName];
+
+param.data_type = 'MEG';
+param.ProjectName = ProjectName;
+param.f_lowpass = 200;
+param.brainstorm_db = ['/dataslow/haoli/brainstorm_db/' ProjectName '/data'];
+
+% classifier parameters
+param.nperm = 100;
+param.kfold = 10;
+param.tgflag = 0;
+param.normalizemean = 1;
+param.normalizestd = 1; 
+param.equalobservations =0;
+
+disp('step 0: parameters set.');
+
+%% step 1: load data at once
+disp('step 1: start loading data...');
+
+condB = '';
+
+for cond = 1:length(condname)
+    [Data{condname(cond)}, Time] = load_trials(param.brainstorm_db,SubjectName,{num2str(condname(cond))},{condB}, param);
+    disp(['loading condition ' num2str(condname(cond)) 'done']);
+end
+param.Time =Time;
+
+%find the mininum n_trials for all conditions
+n_trials = length(Data{condname(1)});
+for cond = 2:length(condname)
+    n_trials_temp = length(Data{condname(cond)});
+    if n_trials > n_trials_temp
+        n_trials = n_trials_temp;
+    end
+end
+
+disp('step 1: loading data done.');
+
+%% step 2: baseline standard deviation correction
+disp('step 2: start baseline standard deviation correction...');
+
+baseline_range = Time<0;
+for cond = 1:length(condname)
+    for i_trial = 1:n_trials
+        Data{cond}{i_trial} =  Data{cond}{i_trial} ./ repmat( std( Data{cond}{i_trial}(:,baseline_range)')',1,size(Time,2) );
+    end
+end
+
+disp('baseline standard deviation correction done.');
+
+%% step 3: compute induced response
+% disp('step 3: computing induced response...');
+% 
+% %initialize Data_induced
+% Data_induced = cell(size(Data));
+% for cond = 1:length(condname)
+%     Data_induced{cond} = cell(1,n_trials);
+%     for i_trial = 1:n_trials
+%         Data_induced{cond}{i_trial} = zeros(size(Data{1}{1}));
+%     end 
+% end
+% 
+% for cond = 1:length(condname)
+%     %compute evoked response
+%     evoked_sum = zeros(size(Data{cond}{1}));
+%     for i_trial = 1:n_trials
+%         evoked_sum = evoked_sum + Data{cond}{i_trial};
+%     end
+%     trial_avg = evoked_sum / n_trials;
+%     %compute induced response
+%     for i_trial = 1:n_trials
+%         Data_induced{cond}{i_trial} = Data{cond}{i_trial} - trial_avg;
+%     end
+% end
+% 
+% clear Data; %save memory!
+% disp('induced response computation done.');
+
+for F = Freq_range
+%% step 4: wavelet transformation
+disp('step 4: start time-frequency transformation...');
+
+fc = 1;
+FWHM_tc = 3;
+
+%initialize Timefreq_induced
+% Timefreq_induced = cell(1,length(condname));
+% for cond = 1:length(condname)
+%     Timefreq_induced{cond} = cell(1,n_trials);
+%     for i_trial = 1:n_trials
+%         Timefreq_induced{cond}{i_trial} = zeros(size(Data{1}{1}));
+%     end
+% end
+
+parfor cond = 1:length(condname) %change to parfor later
+    for i_trial = 1:n_trials
+        temp = squeeze(morlet_transform(Data{cond}{i_trial},Time,F,fc,FWHM_tc,'y'));
+        Timefreq_induced{cond}{i_trial} = temp(:,201:1000); %define case by case!
+        disp(['condition ' num2str(cond) ' trial ' num2str(i_trial) ' done']);
+    end
+    disp(['time-frequency transforamtion condition ' num2str(cond) 'done']);
+end
+disp('time-frequency transformation done.');
+
+%% step 5: SVM decoding
+display('step 5: SVM decoding...');
+
+param.baselineTime =[-0.200 -0.001];%change baseline range after cutting!
+[Accuracy] = pairwise_classification_svm(Timefreq_induced,param);
+
+display('step 5: SVM decoding done.');
+%% step 6: save results
+display('step 6: saving Timefreq data');
+%save( [results_location '/Fig0_Timefreq/mat/Timefreq_freq=' num2str(F)],'Timefreq_induced','Time','param');
+save( [results_location '/Fig1_Accuracy/mat/Total_Accuracy_freq=' num2str(F)],'Accuracy');
+
+disp(['======Freq = ' num2str(F) ' is done!======' ]);
+end
---
+++ codes/TFStep01_Accuracy2Structure_Total.m
@@ -0,0 +1,95 @@
+clear; clc;
+
+%% parameters
+ProjectName = 'grating';  % 'grating03 to grating 16'
+condNum = 18;
+i_subject = 1;
+file_location = ['/dataslow/haoli/workspace/project_grating/results/' ProjectName '_' num2str(i_subject, '%.2d') '/Fig1_Accuracy/mat/'];
+addpath(genpath('Functions')); % add path of functions
+frequency_range = 10:2:80;
+disp_prob_range = [50 70];
+row_index = 0;
+save_fig_location = ['/dataslow/haoli/workspace/project_grating/results/' ProjectName '_' num2str(i_subject, '%.2d') '/Fig1_Accuracy/fig/'];
+save_map_location = ['/dataslow/haoli/workspace/project_grating/results/' ProjectName '_' num2str(i_subject, '%.2d') '/Fig1_Accuracy/mat/map/'];
+onset_time = 200;
+end_time = 700;
+
+
+for F = frequency_range
+    row_index = row_index + 1;
+    %% load result_01
+    load([file_location '/Total_Accuracy_freq=' num2str(F)], 'Accuracy');
+    time_length = size(Accuracy,2);
+    
+    %% average all data at one time point
+    Accuracy1 = sum(Accuracy)/(condNum*(condNum-1)/2);
+    % figure();
+    % plot(Accuracy1(1,:));
+    TimeFreq_Map1(row_index,:) = Accuracy1(1,:);
+    
+    %% average within data at one time point
+    Accuracy_unwrapped = fl_unwrap_conditions(Accuracy);
+    
+    Accuracy_group1 = Accuracy_unwrapped(1:condNum/3, 1:condNum/3, :);
+    Accuracy_group2 = Accuracy_unwrapped(condNum/3+1:condNum*2/3, condNum/3+1:condNum*2/3,:);
+    Accuracy_group3 = Accuracy_unwrapped(condNum*2/3+1:condNum, condNum*2/3+1:condNum, :);
+    
+    Accuracy_group1_average = sum(reshape(Accuracy_group1,(condNum/3)^2,time_length))/((condNum/3)*((condNum/3)-1));
+    %figure();
+    %plot(Accuracy_group1_average(1,:));
+    TimeFreq_Map2(row_index,:) = Accuracy_group1_average(1,:);
+    
+    Accuracy_group2_average = sum(reshape(Accuracy_group2,(condNum/3)^2,time_length))/((condNum/3)*((condNum/3)-1));
+    %figure();
+    %plot(Accuracy_group2_average(1,:));
+    TimeFreq_Map3(row_index,:) = Accuracy_group2_average(1,:);
+    
+    Accuracy_group3_average = sum(reshape(Accuracy_group3,(condNum/3)^2,time_length))/((condNum/3)*((condNum/3)-1));
+    %figure();
+    %plot(Accuracy_group3_average(1,:));
+    TimeFreq_Map4(row_index,:) = Accuracy_group3_average(1,:);
+    
+    Accuracy_group3_average_strong = zeros(1,time_length);
+    for t = 1:time_length
+        asum = 0;
+        for m = 1:(condNum/3)
+            for n = 1:(condNum/3)
+                if (( m ==2 )&&( n == 1 ))||(( m == 4 )&&( n == 3 ))||(( m == 6 )&&( n == 5 ));
+                    asum = asum + Accuracy_group3(m,n,t);
+                end
+            end
+        end
+        Accuracy_group3_average_strong(1,t) = asum/3;
+    end
+    
+    %figure();
+    %plot(Accuracy_group3_average_strong(1,:));
+    TimeFreq_Map5(row_index,:) = Accuracy_group3_average_strong(1,:);
+end
+
+%% show figures
+figure(1);imagesc(TimeFreq_Map1);set(gca,'YDir','normal');set(gca,'ytick',1:length(frequency_range),'yticklabel',frequency_range);colorbar;colormap jet;caxis(disp_prob_range);title('Total Response-All Groups');
+hold on; plot([onset_time onset_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+hold on; plot([end_time end_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+figure(2);imagesc(TimeFreq_Map2);set(gca,'YDir','normal');set(gca,'ytick',1:length(frequency_range),'yticklabel',frequency_range);colorbar;colormap jet;caxis(disp_prob_range);title('Total Response-Group 1(B/W)');
+hold on; plot([onset_time onset_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+hold on; plot([end_time end_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+figure(3);imagesc(TimeFreq_Map3);set(gca,'YDir','normal');set(gca,'ytick',1:length(frequency_range),'yticklabel',frequency_range);colorbar;colormap jet;caxis(disp_prob_range);title('Total Response-Group 2(R/G)');
+hold on; plot([onset_time onset_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+hold on; plot([end_time end_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+figure(4);imagesc(TimeFreq_Map4);set(gca,'YDir','normal');set(gca,'ytick',1:length(frequency_range),'yticklabel',frequency_range);colorbar;colormap jet;caxis(disp_prob_range);title('Total Response-Group 3(Swapped)-All');
+hold on; plot([onset_time onset_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+hold on; plot([end_time end_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+figure(5);imagesc(TimeFreq_Map5);set(gca,'YDir','normal');set(gca,'ytick',1:length(frequency_range),'yticklabel',frequency_range);colorbar;colormap jet;caxis(disp_prob_range);title('Total Response-Group 3(Swapped)-Strong');
+hold on; plot([onset_time onset_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+hold on; plot([end_time end_time],[0 length(frequency_range)+1],'r','Linewidth',3);
+
+%% save figures
+saveas(1,[save_fig_location 'total_response_all_groups.bmp']);
+saveas(2,[save_fig_location 'total_response_group_1_bw.bmp']);
+saveas(3,[save_fig_location 'total_response_group_2_gr.bmp']);
+saveas(4,[save_fig_location 'total_response_group_3_all.bmp']);
+saveas(5,[save_fig_location 'total_response_group_3_strong.bmp']);
+
+%% save mat data
+save([save_map_location 'total_time_freq_decoding_map'],'TimeFreq_Map1','TimeFreq_Map2','TimeFreq_Map3','TimeFreq_Map4','TimeFreq_Map5');
